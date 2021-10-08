3c6147857a08aada6ef81ed611e9d94285e8e8e6
Olga Lipina <neon_alfa@mail.ru>
2021-10-09 00:16:26 +0300

    Version 2.0 Столбец q/m, формат вывода, график q/m от V

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import numpy as np
 import pandas as pd
 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 from PIL import Image
+import os
 from os import listdir
 from os.path import isfile, join
 
+
+# параметры установки
+R = 4.5 / 1000
+g = 9.8
+x_from_left = 1490 # pixels
+y_from_top = 865
 
 def get_imgbw_data(img_file_name):
     img_file = Image.open(img_file_name)
@@ -28,7 +38,7 @@ def count_coords(imgbw_data):
         if item != 0:
             x.append(x_pos)
             y.append(y_pos)
-    xy = np.column_stack((x, y))
+    # xy = np.column_stack((x, y))
     x = np.array(x)
     y = np.array(y)
     return (x.mean().round(2), y.mean().round(2))
@@ -36,7 +46,8 @@ def count_coords(imgbw_data):
 
 if __name__ == "__main__":
     # получение всех изображений из папки
-    path = input("Введите папку для обработки , например 'imgs', 'dataset/photos', etc.\n" )
+   # path = input("Введите папку для обработки , например 'imgs', 'dataset/photos', etc.\n" )
+    path = 'imgs'
     imgs = [f for f in listdir(path) if isfile(join(path, f))]
     imgs_coomp_names = [f.split('.')[0].split('_') for f in imgs]
 
@@ -49,23 +60,37 @@ if __name__ == "__main__":
 
 
     particle_nums = [i[1] for i in imgs_coomp_names]
+    voltage_dc = [float(i[2])/10 for i in imgs_coomp_names]
     iteration_num = [i[-1] for i in imgs_coomp_names]
-    smt_frequency_value = [i[-2] for i in imgs_coomp_names]
-    else_smt = [i[2] for i in imgs_coomp_names]
 
-    xs = [i[0] for i in coords]
-    ys = [i[1] for i in coords]
+    xs = np.array([i[0] for i in coords])
+    ys = np.array([i[1] for i in coords])
 
     whole_info = pd.DataFrame({
         'Particle_num': particle_nums,
+        'Voltage_DC': voltage_dc,
         'Iteration_num': iteration_num,
-        'Smt_freqval': smt_frequency_value,
-        'else_smt': else_smt,
         'x': xs,
         'y': ys
     })
-    info_mean = whole_info.groupby(['else_smt', 'Smt_freqval', 'Particle_num']).mean()[['x', 'y']]
+    info_mean = whole_info.groupby(['Particle_num', 'Voltage_DC']).mean()[['x', 'y']].reset_index()
 
-    whole_info.to_csv('whole_info_2.csv')
-    info_mean.to_csv('info_mean_2.csv')
+    info_mean['Q/m'] = ( g * (R**2 + info_mean.x**2)**(3/2) ) / (info_mean.Voltage_DC*R)
+
+    info_mean.x = info_mean.x.round(2)
+    info_mean.y = info_mean.y.round(2)
+    info_mean['Q/m'] = info_mean.x.round(2)
+
+    print(info_mean)
+
+
+    if os.path.exists('info_mean.csv'):
+        existed_df = pd.read_csv('info_mean.csv')
+        info_mean = pd.concat([existed_df, info_mean]).drop_duplicates().reset_index(drop=True)
+
+    info_mean.to_csv('info_mean.csv')
+
+    sns.scatterplot(data=info_mean, x='Voltage_DC', y='Q/m' )
+    plt.show()
+
 
