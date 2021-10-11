90bb33e731ca1bd93ee61e0f8412c77664885815
Olga Lipina <neon_alfa@mail.ru>
2021-10-11 15:33:12 +0300

    Функциональный подход

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 from PIL import Image
 import os
@@ -13,8 +14,9 @@ from os.path import isfile, join
 # параметры установки
 R = 4.5 / 1000
 g = 9.8
-x_from_left = 1490 # pixels
-y_from_top = 865
+pix_in_met = 9400
+# x_from_left = 1490 # pixels
+# y_from_top = 865
 
 def get_imgbw_data(img_file_name):
     img_file = Image.open(img_file_name)
@@ -39,33 +41,30 @@ def count_coords(imgbw_data):
             x.append(x_pos)
             y.append(y_pos)
     # xy = np.column_stack((x, y))
-    x = np.array(x)
-    y = np.array(y)
-    return (x.mean().round(2), y.mean().round(2))
+    x = np.array(x) / pix_in_met
+    y = np.array(y) / pix_in_met
+    return (x.mean(), y.mean())
 
-
-if __name__ == "__main__":
-    # получение всех изображений из папки
-   # path = input("Введите папку для обработки , например 'imgs', 'dataset/photos', etc.\n" )
-    path = 'imgs'
-    imgs = [f for f in listdir(path) if isfile(join(path, f))]
+def process_file_names(imgs):
     imgs_coomp_names = [f.split('.')[0].split('_') for f in imgs]
+    particle_nums = [i[1] for i in imgs_coomp_names]
+    voltage_dc = [float(i[2]) / 10 for i in imgs_coomp_names]
+    iteration_num = [i[-1] for i in imgs_coomp_names]
+    return particle_nums, voltage_dc, iteration_num
 
-    # конвертация изображений в чёрно-белое
+def get_coords(imgs):
     coords = []
-    for img in imgs:
+    for img in tqdm(imgs):
         imgbw_data = get_imgbw_data(f'{path}/{img}')
         xy = count_coords(imgbw_data)
         coords.append(xy)
+    return coords
 
-
-    particle_nums = [i[1] for i in imgs_coomp_names]
-    voltage_dc = [float(i[2])/10 for i in imgs_coomp_names]
-    iteration_num = [i[-1] for i in imgs_coomp_names]
-
+def build_output(imgs):
+    coords = get_coords(imgs)
     xs = np.array([i[0] for i in coords])
     ys = np.array([i[1] for i in coords])
-
+    particle_nums, voltage_dc, iteration_num = process_file_names(imgs)
     whole_info = pd.DataFrame({
         'Particle_num': particle_nums,
         'Voltage_DC': voltage_dc,
@@ -75,11 +74,22 @@ if __name__ == "__main__":
     })
     info_mean = whole_info.groupby(['Particle_num', 'Voltage_DC']).mean()[['x', 'y']].reset_index()
 
-    info_mean['Q/m'] = ( g * (R**2 + info_mean.x**2)**(3/2) ) / (info_mean.Voltage_DC*R)
+    info_mean['Q/m'] = (g * (R ** 2 + info_mean.x ** 2) ** (3 / 2)) / (info_mean.Voltage_DC * R)
 
-    info_mean.x = info_mean.x.round(2)
-    info_mean.y = info_mean.y.round(2)
-    info_mean['Q/m'] = info_mean.x.round(2)
+    info_mean.x = info_mean.x.round(4)
+    info_mean.y = info_mean.y.round(4)
+    info_mean['Q/m'] = info_mean.x.round(6)
+
+    return info_mean
+
+
+if __name__ == "__main__":
+    # получение всех изображений из папки
+   # path = input("Введите папку для обработки , например 'imgs', 'dataset/photos', etc.\n" )
+    path = 'imgs'
+    imgs = [f for f in listdir(path) if isfile(join(path, f))]
+
+    info_mean = build_output(imgs)
 
     print(info_mean)
 
